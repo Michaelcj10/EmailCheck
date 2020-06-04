5e404b37e19b9577aad1e32d886aa8e01d758dec
michael.james <michael.james@ding.com>
2020-06-04 15:09:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import {
+  SearchOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+  CheckOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+import {
+  Input,
+  Row,
+  Col,
+  Button,
+  Form,
+  Typography,
+  PageHeader,
+  Statistic,
+  Card,
+} from "antd";
+import {
+  defaultDomains,
+  defaultTopLevelDomains,
+  defaultSecondLevelDomains,
+} from "./emailDomains";
+import InfoCard from "./infoCard";
+const { Text, Title } = Typography;
+var mailcheck = require("mailcheck");
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [suggestion, setSuggestion] = useState(null);
+  const [checking, setChecking] = useState(false);
+  const [info, setInfo] = useState(null);
+  const [emailLooksValid, setValidEmail] = useState(false);
+
+  function onSubmit() {
+    if (email === "") {
+      if (error !== "") {
+        setError("Stop that!");
+      } else {
+        setError("Please enter an email");
+      }
+
+      return;
+    }
+
+    setChecking(true);
+    mailcheck.run({
+      email: email,
+      domains: defaultDomains, // optional
+      topLevelDomains: defaultTopLevelDomains, // optional
+      secondLevelDomains: defaultSecondLevelDomains, // optional
+      suggested: function (suggestion) {
+        // callback code
+        console.log(suggestion, "x");
+        setChecking(false);
+        if (suggestion) {
+          setSuggestion(suggestion);
+        }
+      },
+      empty: function (x) {
+        setError("No suggestion for this email");
+        setChecking(false);
+        setInfo("What does this mean?");
+        var x = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var y = x.test(email);
+        setValidEmail(y);
+      },
+    });
+  }
+  return (
+    <>
+      <PageHeader
+        className="site-page-header"
+        title="Email"
+        subTitle="checker"
+      />
+      <div className="App">
+        <Row>
+          <Col
+            xs={{ span: 20, offset: 2 }}
+            md={{ span: 8, offset: 8 }}
+            lg={{ span: 6, offset: 9 }}
+          >
+            <Title style={{ marginBottom: "10px" }} level={4}>
+              Enter an email
+            </Title>
+            <Form>
+              <Input
+                allowClear
+                onPressEnter={onSubmit}
+                disabled={checking}
+                placeholder="Enter email"
+                onChange={(val) => {
+                  if (error !== "") {
+                    setError("");
+                  }
+                  setSuggestion(null);
+                  setEmail(val.currentTarget.value);
+                }}
+                value={email}
+              />
+              {suggestion ? (
+                <>
+                  <span>Did you mean</span>
+                  <Button
+                    type="link"
+                    onClick={() => {
+                      setEmail(suggestion.full);
+                      setSuggestion(null);
+                    }}
+                  >
+                    {suggestion.full}
+                  </Button>
+                </>
+              ) : null}
+
+              {error !== "" ? <Text type="danger">{error}</Text> : null}
+              {info !== null && email !== "" && error !== "" ? (
+                <>
+                  <br />
+                  <span>
+                    Your email looks{" "}
+                    {emailLooksValid ? <CheckOutlined /> : <CloseOutlined />}
+                  </span>
+                </>
+              ) : null}
+
+              <Button
+                disabled={checking}
+                onClick={onSubmit}
+                style={{ marginTop: "25px", width: "100%" }}
+                type="primary"
+                icon={<SearchOutlined />}
+              >
+                Check
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <InfoCard />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/infoCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
+import { Row, Col, Typography, Statistic, Card } from "antd";
+const { Title } = Typography;
+
+function InfoCard() {
+  return (
+    <Row>
+      <Col
+        xs={{ span: 20, offset: 2 }}
+        md={{ span: 8, offset: 8 }}
+        lg={{ span: 6, offset: 9 }}
+      >
+        <div style={{ marginTop: "50px" }}>
+          <Title level={4}>Mailcheck bundle info</Title>
+          <div className="site-statistic-demo-card">
+            <Row gutter={16}>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="BUNDLE SIZE"
+                    value={3.5}
+                    precision={2}
+                    valueStyle={{ color: "#cf1322" }}
+                    prefix={<ArrowDownOutlined />}
+                    suffix="kb"
+                  />
+                </Card>
+              </Col>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="BUNDLE MINIFIED"
+                    value={1.4}
+                    precision={2}
+                    valueStyle={{ color: "#3f8600" }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="kb"
+                  />
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export default InfoCard;
